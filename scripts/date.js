ec13916da020d579495ff343c55754b788276bc0
rmenram <rmendozaramos@byupathway.edu>
2025-07-07 00:38:32 -0700

    week1 check in jquery try to remove

--- scripts/date.js
+++ scripts/date.js
@@ -5,5 +5,5 @@ const year = document.querySelector("#year");
 const today = new Date();
 let last = new Date(document.lastModified);
 
-lastModified.innerHTML = `Last Modification: ${last.toLocaleDateString()} ${last.toLocaleTimeString('en-US', { hour12: false })}`;
-year.innerHTML = `© ${today.getFullYear()}`;
+lastModified.innerHTML = "Last Modification: " + last.toLocaleDateString() + " " + last.toLocaleTimeString('en-US', { hour12: false });
+year.innerHTML = "© " + today.getFullYear();
